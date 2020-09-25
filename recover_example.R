d1ca5dc83e8de18a8b1dd6886d95aa8e20514d38
Yanan Qin <65427957+yananq@users.noreply.github.com>
2020-09-25 17:12:14 -0400

    Delete recover_example.R

--- recover_example.R
+++
@@ -1,8 +0,0 @@
-# this R code is for probe intensity recovery 
-# author: Yanan Qin
-library('affxparser')
-celpath = './GSM707032.CEL'  # change the path of your CEL file
-a = readCelRectangle(as.character(celpath), xrange=c(0, Inf), yrange=c(0, Inf), asMatrix=TRUE, readOutliers=FALSE)
-write.table(a$intensities, file='to_recover.txt', row.names=FALSE, col.names=FALSE)
-
-
